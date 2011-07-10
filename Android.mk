33668d60f95c0cbcd26cce085c3194c2ea64cd5f
Gaurav Aggarwal <gaurav@bluestacks.com>
2011-07-10 17:45:51 +0530

    Don't build Speech Recorder Application

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng
+# Bluestacks - don't build this app 
+LOCAL_MODULE_TAGS := tests
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) \
 
